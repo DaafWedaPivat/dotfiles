d38545ebc64ff54cb546daed626f4475f4e569a5
DaafWedaPivat <daafwedapivat@reckwell.de>
2026-02-18 21:28:39 +0100

    initial dump of current configs

---
+++ .local/bin/hyprpicker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Available formats
+FORMATS="hex\nrgb\nhsl\nhsv\ncmyk"
+
+# Show wofi menu
+FORMAT=$(echo -e "$FORMATS" | rofi -dmenu -p "Format")
+
+# Exit if nothing selected
+[ -z "$FORMAT" ] && exit 0
+
+# Run hyprpicker with selected format
+hyprpicker -a -f "$FORMAT"
